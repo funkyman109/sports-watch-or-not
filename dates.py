5a3b21e9276048014b533ba0d1d395a3d5a76fd8
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-05-03 15:32:15 -0400

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import urllib.request
 
-sauce = urllib.request.urlopen('https://www.foxsports.com/soccer/aston-villa-team-schedule').read()
+sauce = urllib.request.urlopen('https://www.fool.com/investing/top-stocks-to-buy.aspx').read()
 soup= bs.BeautifulSoup(sauce, 'lxml')
 print(soup)
+
